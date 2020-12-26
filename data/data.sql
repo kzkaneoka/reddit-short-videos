6f4c3b7dc09cdad08a996300b7095c732f825438
kzkaneoka <kzkaneoka@gmail.com>
2020-12-26 15:58:54 +0900

    Updated system-design.png

--- data/data.sql
+++ data/data.sql
@@ -5,7 +5,7 @@ CREATE DATABASE reddit_short_videos;
 -- \connect reddit_short_videos
 
 CREATE TYPE e_role AS ENUM('user', 'admin');
-CREATE TYPE e_rate AS ENUM('like', 'dislike', 'skip');
+CREATE TYPE e_rate AS ENUM('like', 'okay', 'dislike', 'skip');
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
@@ -21,18 +21,22 @@ CREATE TABLE users (
 
 CREATE TABLE subreddits (
    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-   name VARCHAR ( 255 ) NOT NULL,
-   url VARCHAR ( 255 ) UNIQUE NOT NULL,
+   title VARCHAR ( 255 ) NOT NULL,
+   s_url VARCHAR ( 255 ) UNIQUE NOT NULL,
    created_at TIMESTAMP DEFAULT NOW(),
    last_modified TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE videos (
    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-   name VARCHAR ( 255 ) NOT NULL,
-   url VARCHAR ( 255 ) NOT NULL,
+   title VARCHAR ( 255 ) NOT NULL,
+   v_url VARCHAR ( 255 ) NOT NULL,
+   r_url VARCHAR ( 255 ) NOT NULL,
+   subreddit_id UUID NOT NULL,
+   posted_at TIMESTAMP NOT NULL,
    created_at TIMESTAMP DEFAULT NOW(),
-   last_modified TIMESTAMP DEFAULT NOW()
+   last_modified TIMESTAMP DEFAULT NOW(),
+   FOREIGN KEY ( subreddit_id ) REFERENCES subreddits ( id )
 );
 
 CREATE TABLE r_users_subreddits (
@@ -45,19 +49,10 @@ CREATE TABLE r_users_subreddits (
    FOREIGN KEY ( subreddit_id ) REFERENCES subreddits ( id )
 );
 
-CREATE TABLE r_subreddits_videos (
-   subreddit_id UUID NOT NULL,
-   video_id UUID NOT NULL,
-   created_at TIMESTAMP DEFAULT NOW(),
-   last_modified TIMESTAMP DEFAULT NOW(),
-   PRIMARY KEY ( subreddit_id, video_id ),
-   FOREIGN KEY ( subreddit_id ) REFERENCES subreddits ( id ),
-   FOREIGN KEY ( video_id ) REFERENCES videos ( id )
-);
-
 CREATE TABLE r_users_videos (
    user_id UUID NOT NULL,
    video_id UUID NOT NULL,
+   rate e_rate NOT NULL,
    created_at TIMESTAMP DEFAULT NOW(),
    last_modified TIMESTAMP DEFAULT NOW(),
    PRIMARY KEY ( user_id, video_id ),
@@ -95,8 +90,8 @@ VALUES
 RETURNING *;
 
 INSERT INTO subreddits (
-   name,
-   url
+   title,
+   s_url
 )
 VALUES
    (
@@ -116,5 +111,24 @@ VALUES
       'https://www.reddit.com/r/nextfuckinglevel'
    )
 RETURNING *;
+
+--                   id                  |           title           |                     s_url                     |         created_at         |       last_modified        
+-- --------------------------------------+---------------------------+-----------------------------------------------+----------------------------+----------------------------
+--  07a0711c-e9bc-472a-b0bb-9c04016a1408 | Damn, that's interesting! | https://www.reddit.com/r/Damnthatsinteresting | 2020-12-26 15:56:13.546286 | 2020-12-26 15:56:13.546286
+--  c4eea120-ea61-4e7c-8f8a-ef73fbbd846a | Funny                     | https://www.reddit.com/r/funny                | 2020-12-26 15:56:13.546286 | 2020-12-26 15:56:13.546286
+--  0cabf079-9929-4aa5-8001-f8b6c57508eb | Oddly Satisfying          | https://www.reddit.com/r/oddlysatisfying      | 2020-12-26 15:56:13.546286 | 2020-12-26 15:56:13.546286
+--  4b8c11a5-5d9c-4477-a569-cb5258b1bc2e | ⬆ Next Fucking Level ⬆    | https://www.reddit.com/r/nextfuckinglevel     | 2020-12-26 15:56:13.546286 | 2020-12-26 15:56:13.546286
+
+INSERT INTO videos (
+   title,
+   v_url,
+   r_url,
+   subreddit_id,
+   posted_at
+)
+VALUES (
+   ''
+)
+
 
 -- brew services stop postgresql
