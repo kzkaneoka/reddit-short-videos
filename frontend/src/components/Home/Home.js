4f4e24437af54786c289fe619c9b2e2a2de5a2c0
관해 (Jack) Park <jkhpark23@utexas.edu>
2021-01-03 01:06:54 +0900

    add filtering videos by subreddit functionality

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Icon, IconButton, Spinner, Flex, Box } from "@chakra-ui/react";
 import { FiArrowRightCircle, FiArrowLeftCircle } from "react-icons/fi";
+import Filter from "./Filter/Filter";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import Ratings from "./Ratings/Ratings";
 import useFetchVideos from "./useFetchVideos";
@@ -8,7 +9,8 @@ import useFetchVideos from "./useFetchVideos";
 function Home() {
   const [{ videoList, isLoading, isError }, fetchVideo] = useFetchVideos();
   const [vidCount, setVidCount] = useState(0);
-  const [disabled, setDisabled] = useState(false);
+  const [filterList, setFilterList] = useState([]);
+  const [playlist, setPlaylist] = useState(videoList);
 
   useEffect(() => {
     fetchVideo("http://localhost:5000/videos");
@@ -21,13 +23,27 @@ function Home() {
   };
 
   const handleNextVidClick = () => {
-    if (vidCount < videoList.length - 1) {
+    if (vidCount < playlist.length - 1) {
       setVidCount(vidCount + 1);
     }
   };
 
+  useEffect(() => {
+    if (filterList.length === 0) {
+      setPlaylist(videoList);
+    } else {
+      setPlaylist(
+        videoList.filter((vid) => {
+          return filterList.includes(vid.subreddit_id);
+        })
+      );
+      setVidCount(0);
+    }
+  }, [videoList, filterList]);
+
   return (
     <div>
+      <Filter videoList={videoList} setFilterList={setFilterList} />
       <Flex justify="center" mx={3} minW="700px">
         <IconButton
           bg="transparent"
@@ -36,14 +52,13 @@ function Home() {
           icon={<Icon as={FiArrowLeftCircle} boxSize={14} color="#535345" />}
           visibility={vidCount === 0 ? "hidden" : "visible"}
           onClick={handlePrevVidClick}
-          disabled={disabled}
         />
 
-        {videoList.length !== 0 ? (
+        {playlist.length !== 0 ? (
           <VideoPlayer
-            videoList={videoList}
+            playlist={playlist}
             count={vidCount}
-            cantChange={setDisabled}
+            filterList={filterList}
           />
         ) : (
           <Spinner />
@@ -54,9 +69,8 @@ function Home() {
           size={14}
           aria-label="Skip rating video"
           icon={<Icon as={FiArrowRightCircle} boxSize={14} color="#535345" />}
-          visibility={vidCount === videoList.length - 1 ? "hidden" : "visible"}
+          visibility={vidCount === playlist.length - 1 ? "hidden" : "visible"}
           onClick={handleNextVidClick}
-          disabled={disabled}
         />
       </Flex>
       <Ratings />
--- frontend/src/components/Home/VideoPlayer/VideoPlayer.js
+++ frontend/src/components/Home/VideoPlayer/VideoPlayer.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 import { AspectRatio, Spinner } from "@chakra-ui/react";
 
 function VideoPlayer(props) {
-  console.log(props);
   // props.cantChange(false);
+  console.log(props.playlist);
   return (
     <div>
       <h1>
-        #{props.count + 1}: {props.videoList[props.count].title}
+        #{props.count + 1}: {props.playlist[props.count].title}
       </h1>
       <AspectRatio minW="650px" ratio={16 / 9}>
         <iframe
-          title={props.videoList[props.count].title}
-          src={props.videoList[props.count].video_url}
+          title={props.playlist[props.count].title}
+          src={props.playlist[props.count].video_url}
           allowFullScreen
         />
       </AspectRatio>
